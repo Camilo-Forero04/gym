1099f0c5ad5ec79730ce9fffed4acc82c4fd3ab7
Camilo Forero <124638099+Camilo-Forero04@users.noreply.github.com>
2026-03-14 12:24:30 -0500

    Agregando CORS para permitir conexion desde Vercel

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -32,7 +32,7 @@ app = FastAPI(
 # --- CONFIGURACIÓN CORS ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"], # Puertos de Next.js
+    allow_origins=["*"], # Puertos de Next.js
     allow_credentials=True,
     allow_methods=["*"], # Permite GET, POST, PUT, DELETE, etc.
     allow_headers=["*"],
